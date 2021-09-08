19a11dea7f7704103f800360e84766db8216b46a
Yehor <yehorStopkin@gmail.com>
2021-09-09 00:41:01 +0200

    add test for ApplicationStepInfo

--- components/application/stepSelector/__tests__/ApplicationStepInfo.test.js
+++ components/application/stepSelector/__tests__/ApplicationStepInfo.test.js
@@ -17,17 +17,53 @@ describe('ApplicationStepInfo', () => {
         const wrapper = shallowMount(ApplicationStepInfo, { propsData: { data: getData() } });
         expect(wrapper.exists()).toBeTruthy();
         expect(wrapper.element).toMatchSnapshot();
+
+        const rootElement = wrapper.findAll('.application-step-info');
+        expect(rootElement.length).toBe(1);
+
+        const progressBar = wrapper.findAll('.application-step-info__progress-bar');
+        expect(progressBar.length).toBe(1);
+
+        const titleElement = wrapper.findAll('.application-step-info__title');
+        expect(titleElement.length).toBe(1);
+
+        const subtitleElement = wrapper.findAll('.application-step-info__subtitle');
+        expect(subtitleElement.length).toBe(1);
+
+        const detailsElement = wrapper.findAll('.application-step-info__details');
+        expect(detailsElement.length).toBe(1);
+
+        const actionElement = wrapper.findAll('.application-step-info__action');
+        expect(actionElement.length).toBe(2);
     });
 
-    test('component completed', () => {
+    test('check actionText of completed step', () => {
         const wrapper = shallowMount(ApplicationStepInfo, { propsData: { data: getData(0, 0, true) } });
-        expect(wrapper.exists()).toBeTruthy();
-        expect(wrapper.element).toMatchSnapshot();
+
+        const actionTextElement = wrapper.findAll('.application-step-info__action-text');
+        expect(actionTextElement.length).toBe(1);
+        expect(actionTextElement.at(0).text()).toBe(wrapper.vm.doneText);
     });
 
-    test('component half-filled ', () => {
-        const wrapper = shallowMount(ApplicationStepInfo, { propsData: { data: getData(0, 50) } });
-        expect(wrapper.exists()).toBeTruthy();
-        expect(wrapper.element).toMatchSnapshot();
+    test('check actionText of uncompleted step', () => {
+        const wrapper = shallowMount(ApplicationStepInfo, { propsData: { data: getData() } });
+
+        const actionTextElement = wrapper.findAll('.application-step-info__action-text');
+        expect(actionTextElement.length).toBe(1);
+        expect(actionTextElement.at(0).text()).toBe(wrapper.vm.completeText);
+    });
+
+    test('check select step', () => {
+        const wrapper = shallowMount(ApplicationStepInfo, { propsData: { data: getData(2) } });
+
+        const actionTextElement = wrapper.findAll('.application-step-info__action-text');
+        actionTextElement.at(0).trigger('click');
+
+        expect(wrapper.emitted().selected[0][0]).toEqual(wrapper.vm.data.stepNumber);
+    });
+
+    test('check progress getter', () => {
+        const wrapper = shallowMount(ApplicationStepInfo, { propsData: { data: getData() } });
+        expect(wrapper.vm.progress).toBe(wrapper.vm.data.filingPercentage);
     });
 });
