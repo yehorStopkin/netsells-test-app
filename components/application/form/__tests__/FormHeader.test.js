83074a5753d9736926c0182598ce82fe9b88efcc
Yehor <yehorStopkin@gmail.com>
2021-09-09 00:54:09 +0200

    add tests for FormHeader

--- components/application/form/__tests__/FormHeader.test.js
+++ components/application/form/__tests__/FormHeader.test.js
@@ -2,9 +2,34 @@ const { shallowMount } = require('@vue/test-utils');
 const FormHeader = require('@/components/application/form/FormHeader').default;
 
 describe('FormHeader', () => {
+    const title = 'Test title';
+
     test('component default', () => {
-        const wrapper = shallowMount(FormHeader, { propsData: { title: 'Test title' } });
+        const wrapper = shallowMount(FormHeader, { propsData: { title } });
         expect(wrapper.exists()).toBeTruthy();
         expect(wrapper.element).toMatchSnapshot();
+
+        const rootElement = wrapper.findAll('.form-header');
+        expect(rootElement.exists()).toBeTruthy();
+
+        const titleElement = wrapper.findAll('.form-header__title');
+        expect(titleElement.length).toBe(1);
+
+        const subtitleElement = wrapper.findAll('.form-header__subtitle');
+        expect(subtitleElement.length).toBe(2);
+    });
+
+    test('check title text', () => {
+        const wrapper = shallowMount(FormHeader, { propsData: { title } });
+
+        const titleElement = wrapper.find('.form-header__title');
+        expect(titleElement.text()).toBe(wrapper.vm.title);
+    });
+
+    test('check subtitle text', () => {
+        const wrapper = shallowMount(FormHeader, { propsData: { title } });
+
+        const subtitleElement = wrapper.findAll('.form-header__subtitle');
+        expect(subtitleElement.at(1).text()).toBe(wrapper.vm.subtitle);
     });
 });
