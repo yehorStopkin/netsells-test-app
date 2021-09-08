1d98ee3024a5e607782debb473295bd10f810888
Yehor <yehorStopkin@gmail.com>
2021-09-08 13:36:46 +0200

    add test for FormAdditionalFiles

---
+++ components/application/form/__tests__/FormAdditionalFiles.test.js
@@ -0,0 +1,33 @@
+const { shallowMount } = require('@vue/test-utils');
+const FormAdditionalFiles = require('@/components/application/form/FormAdditionalFiles').default;
+
+describe('FormAdditionalFiles', () => {
+    getValue = (cv = null, coverLetter = null) => {
+        return {
+            cv,
+            coverLetter,
+        };
+    };
+
+    const uploadedFile = {
+        name: 'Test file name.pdf',
+    };
+
+    test('component default', () => {
+        const wrapper = shallowMount(FormAdditionalFiles, { propsData: { value: getValue(), step: 0 } });
+        expect(wrapper.exists()).toBeTruthy();
+        expect(wrapper.element).toMatchSnapshot();
+    });
+
+    test('component uploaded cv', () => {
+        const wrapper = shallowMount(FormAdditionalFiles, { propsData: { value: getValue(uploadedFile), step: 0 } });
+        expect(wrapper.exists()).toBeTruthy();
+        expect(wrapper.element).toMatchSnapshot();
+    });
+
+    test('component uploaded cover letter', () => {
+        const wrapper = shallowMount(FormAdditionalFiles, { propsData: { value: getValue(null, uploadedFile), step: 0 } });
+        expect(wrapper.exists()).toBeTruthy();
+        expect(wrapper.element).toMatchSnapshot();
+    });
+});
