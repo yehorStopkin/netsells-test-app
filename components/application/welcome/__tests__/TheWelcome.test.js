6f6e32b40520e5575b32ff6dd76dc86f6ce927c1
Yehor <yehorStopkin@gmail.com>
2021-09-08 23:48:10 +0200

    add tests for TheWelcome

--- components/application/welcome/__tests__/TheWelcome.test.js
+++ components/application/welcome/__tests__/TheWelcome.test.js
@@ -6,5 +6,29 @@ describe('TheWelcome', () => {
         const wrapper = shallowMount(TheWelcome);
         expect(wrapper.exists()).toBeTruthy();
         expect(wrapper.element).toMatchSnapshot();
+
+        const rootElement = wrapper.findAll('.the-welcome');
+        expect(rootElement.length).toBe(1);
+
+        const titleElement = wrapper.findAll('.the-welcome__title');
+        expect(titleElement.length).toBe(1);
+
+        const subtitleElement = wrapper.findAll('.the-welcome__subtitle');
+        expect(subtitleElement.length).toBe(1);
+
+        const actionElement = wrapper.findAll('.the-welcome__actions');
+        expect(actionElement.length).toBe(1);
+    });
+
+    test('check click on button', () => {
+        const wrapper = shallowMount(TheWelcome);
+
+        const button = wrapper.find('*[data-test-id="app-button"]');
+        expect(button.exists()).toBeTruthy();
+
+        button.trigger('clicked');
+
+        wrapper.vm.startEvent = jest.fn();
+        expect(wrapper.vm.startEvent).toHaveBeenCalledTimes(0);
     });
 });
