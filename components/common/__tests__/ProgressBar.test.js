be6a17c4481e545f0d2eddc9984dcf31327356b8
Yehor <yehorStopkin@gmail.com>
2021-09-08 18:35:11 +0200

    add test

--- components/common/__tests__/ProgressBar.test.js
+++ components/common/__tests__/ProgressBar.test.js
@@ -8,33 +8,18 @@ describe('ProgressBar', () => {
         expect(wrapper.exists()).toBeTruthy();
         expect(wrapper.element).toMatchSnapshot();
 
-        const emptyBar = wrapper.find('.js-progress-bar__empty');
-        const filledBar = wrapper.find('.js-progress-bar__filled');
+        const emptyBar = wrapper.find('.progress-bar__empty');
+        const filledBar = wrapper.find('.progress-bar__filled');
 
         expect(emptyBar.exists()).toBeTruthy();
         expect(filledBar.exists()).toBeTruthy();
     });
     
-    test('half-filled bar', () => {
-        const progress = 50;
-        const wrapper = shallowMount(ProgressBar, { propsData: { progress } });
-
-        expect(wrapper.exists()).toBeTruthy();
-        expect(wrapper.element).toMatchSnapshot();
-
-        const filledBar = wrapper.find('.js-progress-bar__filled');
-
-        expect(filledBar.attributes('style') === `width: ${ progress }%`);
-    });
-
     test('full-filled bar', () => {
         const progress = 100;
         const wrapper = shallowMount(ProgressBar, { propsData: { progress } });
 
-        expect(wrapper.exists()).toBeTruthy();
-        expect(wrapper.element).toMatchSnapshot();
-
-        const filledBar = wrapper.find('.js-progress-bar__filled');
+        const filledBar = wrapper.find('.progress-bar__filled');
 
         expect(filledBar.attributes('style') === `width: ${ progress }%`);
     });
